767995033dc0116375f742f1d9ee831d8a1f77fb
dabutskikh <dabutskikh@gmail.com>
2024-02-13 16:02:40 +0400

    Изменил алгоритм генерации логина пользователя для хранения в БД. Для достижения однозначности используется логин аккаунта в Телеграме (является уникальным) или ID аккаунта (при отсутствии логина Telegram). Реализовал метод поиска игроков по ID партии

--- src/main/java/ru/dabutskikh/monopolytelegrambot/repository/PlayerRepository.java
+++ src/main/java/ru/dabutskikh/monopolytelegrambot/repository/PlayerRepository.java
@@ -4,10 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.dabutskikh.monopolytelegrambot.entity.Player;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
 
     Optional<Player> findByTelegramId(Long telegramId);
+
+    List<Player> findByCurrentGameId(Long gameId);
 }
--- src/main/java/ru/dabutskikh/monopolytelegrambot/service/PlayerService.java
+++ src/main/java/ru/dabutskikh/monopolytelegrambot/service/PlayerService.java
@@ -9,7 +9,11 @@ import ru.dabutskikh.monopolytelegrambot.entity.Player;
 import ru.dabutskikh.monopolytelegrambot.exception.UserException;
 import ru.dabutskikh.monopolytelegrambot.repository.PlayerRepository;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -22,7 +26,13 @@ public class PlayerService {
         checkNotExistsPlayerByTelegramId(dto.getTelegramId());
         Player entity = new Player();
         entity.setTelegramId(dto.getTelegramId());
-        entity.setUsername(dto.getUsername());
+        String telegramUsername = dto.getUsername() != null
+                ? dto.getUsername()
+                : String.valueOf(dto.getTelegramId());
+        String username = Stream.of(dto.getFirstName(), dto.getLastName(), telegramUsername)
+                .filter(Objects::nonNull)
+                .collect(Collectors.joining(" "));
+        entity.setUsername(username);
         entity.setLastName(dto.getLastName());
         entity.setFirstName(dto.getFirstName());
         playerRepository.saveAndFlush(entity);
@@ -45,13 +55,6 @@ public class PlayerService {
         );
     }
 
-    public PlayerDTO getById(Long id) {
-        Player entity = playerRepository.findById(id).orElseThrow(() -> new UserException("Игрока с указанным идентификатором не существует"));
-        PlayerDTO dto = new PlayerDTO();
-        BeanUtils.copyProperties(entity, dto);
-        return dto;
-    }
-
     @Transactional
     public PlayerDTO update(PlayerDTO dto) {
         Player entity = playerRepository.findById(dto.getId())
@@ -59,5 +62,16 @@ public class PlayerService {
         BeanUtils.copyProperties(dto, entity);
         playerRepository.saveAndFlush(entity);
         return dto;
+    }
+
+    public List<PlayerDTO> findByCurrentGameId(Long gameId) {
+        List<Player> entities = playerRepository.findByCurrentGameId(gameId);
+        return entities.stream()
+                .map(player -> {
+                    PlayerDTO dto = new PlayerDTO();
+                    BeanUtils.copyProperties(player, dto);
+                    return dto;
+                })
+                .toList();
     }
 }
